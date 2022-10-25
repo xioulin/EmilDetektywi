187ab0d339530e6d12963169e90dfbb50da2eadc
Piotr Chomicki <109987107+xioulin@users.noreply.github.com>
2022-10-25 10:56:38 -0500

    junit test

--- junit_testing_lecture/src/test/java/CodeupCryptTest.java
+++ junit_testing_lecture/src/test/java/CodeupCryptTest.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class CodeupCryptTest {
+import org.example.CodeupCrypt;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CodeupCryptTest {
+
+    @Test
+    public void testIfVersionIsSet(){
+        assertEquals(0.0, CodeupCrypt.version, 0);
+        CodeupCrypt.version = 1.2;
+        assertEquals(1.2, CodeupCrypt.version, 02);
+    }
+
+    @Test
+    public void testHashPassword(){
+        assertEquals("C0d39p", CodeupCrypt.hashPassword("Codeup"));
+        assertEquals("F3r", CodeupCrypt.hashPassword("Fer"));
+        assertEquals("123", CodeupCrypt.hashPassword("123"));
+        assertEquals("124", CodeupCrypt.hashPassword("12a"));
+    }
+
+    @Test
+    public void testCheckPassword(){
+        assertTrue("check password", CodeupCrypt.checkPassword("fer", "f3r"));
+        assertFalse("check password", CodeupCrypt.checkPassword("fer", "fer"));
+    }
+
 }
--- junit_testing_lecture/src/test/java/TestMain.java
+++ junit_testing_lecture/src/test/java/TestMain.java
@@ -1,6 +1,58 @@
 import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
+public class TestMain {
+    @Test
+    public void testIfCompanyNameIsEquals() {
+        String expected = "Codeup";
+        String actual = "CodeUp";
 
-public class Test {
+//        assertEquals(expected, actual);
+        assertNotEquals(expected,actual);
+    }
+
+// When you're using floating points values a third parameter is required (Delta) which represents a positive margin of error of decimals.
+
+    @Test
+    public void testIfChangeIsCorrect() {
+        Double price = 10.0;
+        Double discount = 4.5;
+
+        assertEquals(5.5, price - discount, 0);
+        assertEquals(5.1, price - discount, 0.5);
+        assertNotEquals(4.9, price - discount, 0.5);
+    }
+
+@Test
+    public void testNotSameArrayList(){
+        List<String> languages = new ArrayList<>();
+        languages.add("hello");
+        languages.remove("hello");
+        List<String> moreLanguages = new ArrayList<>();
+
+        assertNotSame(languages,moreLanguages);
+        assertSame(languages,moreLanguages);
+    }
+
+    @Test
+    public void testIfArraysEqual(){
+        int[] number = {1,2,3};
+        int[] otherNumbers = {1,2,4};
+
+        assertArrayEquals(number,otherNumbers);
+    }
+
+
+    @Test
+    public void TestCharactersinString() {
+        String language = "PHP";
+        assertTrue(language.contains("H"));
+        assertFalse(language.contains("i"));
+    }
 }
+
