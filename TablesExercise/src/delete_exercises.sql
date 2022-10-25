2e82aa5e868530c08e6f63a3d659a91cff3265d0
Piotr Chomicki <109987107+xioulin@users.noreply.github.com>
2022-10-25 15:50:11 -0500

    exercises

---
+++ TablesExercise/src/delete_exercises.sql
@@ -0,0 +1,6 @@
+USE codeup_test_db;
+
+DELETE  FROM albums WHERE release_date >1991;
+DELETE  FROM albums WHERE genre = "Disco";
+DELETE  FROM albums WHERE artist = "Whitney Houston";
+    SELECT * FROM albums;
