9772064dc1af30b7f6f84a0464b336f5ce09c6c6
Piotr Chomicki <109987107+xioulin@users.noreply.github.com>
2023-06-19 18:12:51 -0500

    hello

---
+++ EmilDetektywi/src/main/java/com/example/emildetektywi/EmilDetektywiApplication.java
@@ -0,0 +1,14 @@
+package com.example.emildetektywi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmilDetektywiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmilDetektywiApplication.class, args);
+    }
+
+
+}
---
+++ EmilDetektywi/src/test/java/com/example/emildetektywi/EmilDetektywiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emildetektywi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmilDetektywiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
